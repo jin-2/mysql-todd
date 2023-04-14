1e1bb7970da84c1d7e753975742921457c43fb7b
이수진 <sujinlee2@jobkorea.co.kr>
2023-04-14 18:03:43 +0900

    Node.js와 MySQL 연동

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Node.js에 MySQL 연동](https://poiemaweb.com/nodejs-mysql)
+
+블로그 내용 따라해보기
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("HEY");
+require("dotenv").config();
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB,
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM Users", (errors, rows, fields) => {
+  if (errors) throw errors;
+  console.log("User info is: ", rows);
+});
+
+connection.end();
